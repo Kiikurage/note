65bbb993be4729de90e4e9df12111437c9d6ba60
Yuichiro Kikura <y.kikura@gmail.com>
2024-02-19 03:06:23 +0900

    Update github actions

--- workspaces/textarea-handling/src/lib/assert.ts
+++ workspaces/textarea-handling/src/lib/assert.ts
@@ -1,5 +1,6 @@
 export function assert(condition: boolean, message: string): asserts condition {
     if (!condition) {
+        // eslint-disable-next-line no-debugger
         debugger;
         throw new Error(message);
     }
